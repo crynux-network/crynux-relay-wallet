0ed119bea3c645e9d73d9bc4f1f07b60de9bd887
iwehf <henry.lee@crynux.ai>
2025-08-28 20:26:59 +0800

    change sync task fee logs

--- tasks/sync_task_fee_logs.go
+++ tasks/sync_task_fee_logs.go
@@ -6,6 +6,7 @@ import (
 	"crynux_relay_wallet/models"
 	"crynux_relay_wallet/relay_api"
 	"errors"
+	"fmt"
 	"math/big"
 	"time"
 
@@ -17,21 +18,103 @@ func StartSyncTaskFeeLogs(ctx context.Context) error {
 
 	interval := time.Duration(config.GetConfig().Tasks.SyncTaskFeeLogs.IntervalSeconds) * time.Second
 
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
 	for {
 		select {
 		case <-ctx.Done():
 			log.Infoln("Sync task fee logs task is stopping")
 			return nil
-		case <-time.After(interval):
-			if err := syncTaskFeeLogs(); err != nil {
-				log.Errorln(err)
+		case <-ticker.C:
+			err := func () error {
+				ctx1, cancel := context.WithTimeout(ctx, interval)
+				defer cancel()
+				return syncTaskFeeLogs(ctx1)
+			}()
+			if err != nil {
+				log.Errorf("Failed to sync task fee logs: %v", err)
 				return err
 			}
 		}
 	}
 }
 
-func syncTaskFeeLogs() error {
+func mergeTaskFeeLogs(logs []relay_api.TaskFeeLog) (map[string]*big.Int, error) {
+	merged := make(map[string]*big.Int)
+
+	for _, log := range logs {
+		amount, success := new(big.Int).SetString(log.TaskFee, 10)
+		if !success {
+			return nil, errors.New("cannot parse amount from task fee log")
+		}
+
+		if _, ok := merged[log.Address]; !ok {
+			merged[log.Address] = amount
+		} else {
+			merged[log.Address].Add(merged[log.Address], amount)
+		}
+	}
+
+	return merged, nil
+}  
+
+func processTaskFeeLogs(ctx context.Context, db *gorm.DB, logs []relay_api.TaskFeeLog) error {
+	merged, err := mergeTaskFeeLogs(logs)
+	if err != nil {
+		return err
+	}
+	
+	var addresses []string
+	for address := range merged {
+		addresses = append(addresses, address)
+	}
+
+	
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		var accounts []*models.RelayAccount
+		if err := tx.Model(&models.RelayAccount{}).Where("address IN (?)", addresses).Find(&accounts).Error; err != nil {
+			return err
+		}
+
+		existedAddresses := make(map[string]bool)
+		for _, account := range accounts {
+			existedAddresses[account.Address] = true
+		}
+
+		for _, account := range accounts {
+			amount, ok := merged[account.Address]
+			if !ok {
+				continue
+			}
+			account.Balance.Add(&account.Balance.Int, amount)
+		}
+
+		var newAccounts []*models.RelayAccount
+		for address, amount := range merged {
+			if _, ok := existedAddresses[address]; !ok {
+				newAccounts = append(newAccounts, &models.RelayAccount{Address: address, Balance: models.BigInt{Int: *amount}})
+			}
+		}
+		
+		if err := tx.CreateInBatches(newAccounts, 100).Error; err != nil {
+			return err
+		}
+
+		var cases string
+		for _, account := range accounts {
+			cases += fmt.Sprintf(" WHEN address = '%s' THEN '%s'", account.Address, account.Balance.String())
+		}
+		if err := tx.Model(&models.RelayAccount{}).Where("address IN (?)", existedAddresses).
+			Update("balance", gorm.Expr("CASE"+cases+" END")).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func syncTaskFeeLogs(ctx context.Context) error {
 	db := config.GetDB()
 
 	var system models.System
@@ -49,44 +132,13 @@ func syncTaskFeeLogs() error {
 		return nil
 	}
 
-	return db.Transaction(func(tx *gorm.DB) error {
-		for _, taskFeeLog := range logs {
-			var account models.RelayAccount
-
-			err := tx.Where(models.RelayAccount{Address: taskFeeLog.Address}).First(&account).Error
-			if err != nil {
-				if errors.Is(err, gorm.ErrRecordNotFound) {
-					// create new account
-					amount, success := new(big.Int).SetString(taskFeeLog.Amount, 10)
-					if !success {
-						return errors.New("cannot parse amount from task fee log")
-					}
-					account = models.RelayAccount{
-						Address: taskFeeLog.Address,
-						Balance: models.BigInt{Int: *amount},
-					}
-					if err := tx.Create(&account).Error; err != nil {
-						return err
-					}
-				} else {
-					return err
-				}
-			} else {
-				// update account
-				amount, success := new(big.Int).SetString(taskFeeLog.Amount, 10)
-				if !success {
-					return errors.New("cannot parse amount from task fee log")
-				}
-				newBalance := new(big.Int).Add(&account.Balance.Int, amount)
-				account.Balance = models.BigInt{Int: *newBalance}
-				if err := tx.Save(&account).Error; err != nil {
-					return err
-				}
-			}
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := processTaskFeeLogs(ctx, tx, logs); err != nil {
+			return err
 		}
 
 		system.LatestTaskFeeLogID = logs[len(logs)-1].ID
-		system.LatestTaskFeeLogTimestamp = logs[len(logs)-1].Timestamp
+		system.LatestTaskFeeLogTimestamp = logs[len(logs)-1].CreatedAt
 		return tx.Save(&system).Error
 	})
 }
