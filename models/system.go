ab47c5bbf77f4fa82b4b1c6076fb720aa494bebf
iwehf <henry.lee@crynux.ai>
2025-08-28 20:26:49 +0800

    complete relay api

--- models/system.go
+++ models/system.go
@@ -1,7 +1,7 @@
 package models
 
 type System struct {
-	ID        uint64 `json:"id" gorm:"primarykey"`
-	LatestTaskFeeLogID uint64 `json:"latest_task_fee_log_id"`
+	ID                        uint   `json:"id" gorm:"primarykey"`
+	LatestTaskFeeLogID        uint   `json:"latest_task_fee_log_id"`
 	LatestTaskFeeLogTimestamp uint64 `json:"latest_task_fee_log_timestamp"`
 }
--- relay_api/withdrawal_request.go
+++ relay_api/withdrawal_request.go
@@ -1,12 +1,202 @@
 package relay_api
 
+import (
+	"bytes"
+	"crynux_relay_wallet/config"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type WithdrawStatus int8
+
+const (
+	WithdrawStatusPending WithdrawStatus = iota
+	WithdrawStatusSuccess
+	WithdrawStatusFailed
+)
+
 type WithdrawalRequest struct {
-	ID                uint64
-	Timestamp         uint64
-	Nonce             uint64
-	Blockchain        string
-	Address           string
-	BeneficialAddress string
-	Amount            string
-	Signature         string
+	ID             uint           `json:"id"`
+	CreatedAt      uint64         `json:"created_at"`
+	Address        string         `json:"address"`
+	BenefitAddress string         `json:"benefit_address"`
+	Amount         string         `json:"amount"`
+	Network        string         `json:"network"`
+	Status         WithdrawStatus `json:"status"`
+}
+
+type GetWithdrawalRequestsInput struct {
+	StartID uint `query:"start_id" json:"start_id" description:"Start ID"`
+	Limit   int  `query:"limit" json:"limit" description:"Limit"`
+}
+
+type GetWithdrawalRequestsOutput struct {
+	Data []WithdrawalRequest `json:"data"`
+}
+
+func GetWithdrawalRequests(pivotWithdrawalRequestID uint, limit int) ([]WithdrawalRequest, error) {
+	conf := config.GetConfig()
+
+	req, err := http.NewRequest("GET", conf.Relay.Api.Host+"/v1/withdraw/list", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	input := GetWithdrawalRequestsInput{
+		StartID: pivotWithdrawalRequestID,
+		Limit:   limit,
+	}
+
+	timestamp, signature, err := SignData(input, conf.Blockchain.Account.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	q.Add("start_id", fmt.Sprintf("%d", pivotWithdrawalRequestID))
+	q.Add("limit", fmt.Sprintf("%d", limit))
+	q.Add("timestamp", fmt.Sprintf("%d", timestamp))
+	q.Add("signature", signature)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Errorln(err)
+		}
+	}(resp.Body)
+
+	var output GetWithdrawalRequestsOutput
+
+	if err := json.NewDecoder(resp.Body).Decode(&output); err != nil {
+		return nil, err
+	}
+
+	return output.Data, nil
+}
+
+type FulfillWithdrawalRequestInput struct {
+	ID uint `json:"id"`
+}
+
+type FulfillWithdrawalRequestInputWithSignature struct {
+	FulfillWithdrawalRequestInput
+	Timestamp int64  `form:"timestamp" json:"timestamp" description:"Signature timestamp" validate:"required"`
+	Signature string `form:"signature" json:"signature" description:"Signature" validate:"required"`
+}
+
+func FulfillWithdrawalRequest(withdrawalRequestID uint) error {
+	conf := config.GetConfig()
+
+	
+	input := FulfillWithdrawalRequestInput{
+		ID: withdrawalRequestID,
+	}
+
+	timestamp, signature, err := SignData(input, conf.Blockchain.Account.PrivateKey)
+	if err != nil {
+		return err
+	}
+
+	inputWithSignature := FulfillWithdrawalRequestInputWithSignature{
+		FulfillWithdrawalRequestInput: input,
+		Timestamp: timestamp,
+		Signature: signature,
+	}
+
+	body, err := json.Marshal(inputWithSignature)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/v1/withdraw/%d/fulfill", conf.Relay.Api.Host, withdrawalRequestID), bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Errorln(err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to fulfill withdrawal request: %s", resp.Status)
+	}
+
+	return nil
+}
+
+type RejectWithdrawalRequestInput struct {
+	ID uint `json:"id"`
+}
+
+type RejectWithdrawalRequestInputWithSignature struct {
+	RejectWithdrawalRequestInput
+	Timestamp int64  `form:"timestamp" json:"timestamp" description:"Signature timestamp" validate:"required"`
+	Signature string `form:"signature" json:"signature" description:"Signature" validate:"required"`
+}
+
+func RejectWithdrawalRequest(withdrawalRequestID uint) error {
+	conf := config.GetConfig()
+
+	
+	input := RejectWithdrawalRequestInput{
+		ID: withdrawalRequestID,
+	}
+
+	timestamp, signature, err := SignData(input, conf.Blockchain.Account.PrivateKey)
+	if err != nil {
+		return err
+	}
+
+	inputWithSignature := RejectWithdrawalRequestInputWithSignature{
+		RejectWithdrawalRequestInput: input,
+		Timestamp: timestamp,
+		Signature: signature,
+	}
+
+	body, err := json.Marshal(inputWithSignature)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/v1/withdraw/%d/reject", conf.Relay.Api.Host, withdrawalRequestID), bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Errorln(err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to reject withdrawal request: %s", resp.Status)
+	}
+
+	return nil
 }
